1e0c728de2ed0799838ed82ac7eeb3042af948f5
githukelvin <kelvingithu019@gmail.com>
2024-02-09 18:52:45 +0300

    test if tailwind is configured successfully

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,15 @@
 <!-- eslint-disable no-undef -->
 <script setup lang="ts">
-const route = useRoute()
 
 const appConfig = useAppConfig()
 
-console.log(appConfig)
+console.log(appConfig.theme)
 </script>
 
 <template>
   <div>
-    <h1>Nuxt Routing set up successfully!</h1>
-    <p>Current route: {{ route.path }}</p>
-    <a href="https://nuxt.com/docs/getting-started/routing" target="_blank"
-      >Learn more about Nuxt Routing</a
-    >
+   <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   </div>
 </template>
