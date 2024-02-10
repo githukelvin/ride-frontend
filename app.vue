4d351d60d2bc7dd3441e494d82f3f42e6ff448fa
githukelvin <kelvingithu019@gmail.com>
2024-02-10 20:25:12 +0300

    started implementing logic

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="min-h-screen bg-gray-100 text-center antialised">
     <NuxtPage />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,34 @@
-<!-- eslint-disable no-undef -->
-<script setup lang="ts">
-
-const appConfig = useAppConfig()
-
-console.log(appConfig.theme)
-</script>
-
 <template>
-  <div>
-   <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  <div class="pt-16">
+    <h1 class="text-3xl font-semibold mb-4">Enter your phone number</h1>
+    <form action="#">
+      <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
+        <div class="bg-white px-4 py-5 sm:p-6">
+          <div>
+            <input
+              type="text"
+              name="phone"
+              id="phone"
+              v-maska 
+              data-maska="+254 ## ####-####"
+              placeholder="1 (234) 567-8910"
+              class="mt-1 block w-full px-3 py-2 rounded-md border bg-white border-gray-300 shadow-sm focus:border-black focus:outline-none"
+            />
+          </div>
+        </div>
+        <div class="bg-gray-50 px4 py-3 text-right sm:px-6">
+          <button
+            type="submit"
+            class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none"
+          >
+            Continue
+          </button>
+        </div>
+      </div>
+    </form>
   </div>
 </template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
